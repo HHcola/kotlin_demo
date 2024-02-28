3f354772f3e90a80db9c80e3482a1cf4a8b5e392
p_weihhhe <p_weihhhe@tencent.com>
2024-02-28 11:13:48 +0800

    commit demo

---
+++ src/main/kotlin/flow/flows.kt
@@ -0,0 +1,26 @@
+package flow
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+
+class flows {
+
+    private fun simple(): Flow<Int> = flow { // flow builder
+        for (i in 1..3) {
+            delay(100) // pretend we are doing something useful here
+            emit(i) // emit next value
+        }
+    }
+
+    fun runMain() = runBlocking<Unit> {
+        // Launch a concurrent coroutine to check if the main thread is blocked
+        launch {
+            for (k in 1..3) {
+                println("I'm not blocked $k")
+                delay(100)
+            }
+        }
+        // Collect the flow
+        simple().collect { value -> println(value) }
+    }
+}
